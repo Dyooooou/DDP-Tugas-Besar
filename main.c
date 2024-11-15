c3e7f4bee89e13c7bb074ac3172c970262244201
Dyooooou <juliandiokhusussekolah@gmail.com>
2024-11-15 16:26:26 +0700

    nyoba

--- main.c
+++ main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 
+void MenampilkanFilm();
+int pilihFilm();
+void MenampilkanLokasi();
+int pilihLokasi();
+void MenampilkanJadwal();
+int pilihJadwal();
+void konfirmasiPilihan(int PilihanFilm, int PilihanLokasi, int PilihanJadwal);
+
 int main (){
-  printf("Hellooooo TUGAS");
-return 0;
-//julian bukan jawa
+//   printf("Hellooooo TUGAS");
+// return 0;
+// julian bukan jawa
+    int PilihanFilm, PilihanLokasi, PilihanJadwal;
+    MenampilkanFilm();
+    PilihanFilm = pilihFilm();
+    PilihanLokasi = pilihLokasi();
+    PilihanJadwal = pilihJadwal():
+    konfirmasiPilihan(int PilihanFilm, int PilihanLokasi, int PilihanJadwal);
+}
+
+void MenampilkanFilm();
+
+void MenampilkanFilm(){
+  printf("Daftar Film yang dapat ditonton /n");
+  printf("1. Oshi No Ko");
+  printf("2. The Last of Us");
+  printf("3. DAYZ GONE");
 }
