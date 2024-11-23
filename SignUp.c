03ddf25f2cd093e91552eb2dd117fee07f935234
zah-svg <hanasy171@gmail.com>
2024-11-23 20:44:26 +0700

    Sign Up dan Sign In
    hampir bener (harusnya)

--- SignUp.c
+++ SignUp.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <string.h>
-#include <windows.h>
-#define File_Path "c:\Users\DELL\Documents\tugasddp\simpan data.txt"
+#include <stdlib.h>
+#define Path "c:\\Sign In Sign Up\\database.txt"
 
 void SignUp();
+void SignIn();
 int cekemail();
 int cekpassword();
-char email[50];
-char password[10];
+char email[50]; // var global
+char password[10]; // var global
 
 int main() {
     int pilihan;
@@ -27,9 +28,9 @@ int main() {
     switch(pilihan){
         case 1 : SignUp();
         break;
-        case 2 : printf("SignIn\n");
+        case 2 : SignIn();
         break;
-        default : printf("ANGKA YANG ANDA MASUKKAN TIDAK VALID");
+        default : printf("ANGKA YANG ANDA MASUKKAN TIDAK VALID\n");
         break;
     }
     }while(pilihan != 2);
@@ -49,6 +50,18 @@ void SignUp(){
         fgets(password, sizeof(password), stdin);
         if(cekpassword() == 0){
             goto cek2;
+        }else{
+            FILE *fptr = NULL;
+            fptr = fopen(Path, "a");
+            if(fptr == NULL){
+                system("cls");
+                printf("Coba Lagi");
+            }else{
+                fprintf(fptr, "%s %s\n", email, password);
+                printf("Sign Up Anda Berhasil\n");
+                fclose(fptr);
+                exit(0);
+            }
         }
      }
 }
@@ -69,6 +82,19 @@ int cekemail(){
             return 0;
         }
         i++;
+     }
+     FILE *fptr = NULL;
+     fptr = fopen(Path, "r");
+     if(fptr == NULL){
+        system("cls");
+     }else{
+        while(fscanf(fptr,"%s", email) == 1){
+            if(email == 0){
+                system("cls");
+                printf("Email Telah Terdaftar. Masukkan Email Lain\n");
+                return 0;
+            }
+        }
      }
      return 1;
 }
@@ -91,4 +117,31 @@ int cekpassword(){
         i++;
     }
     return 1;
+}
+
+void SignIn(){
+    while(getchar() != '\n');
+    printf("MASUKKAN EMAIL ANDA: ");
+    fgets(email, sizeof(email), stdin);
+    printf("MASUKKAN PASSWORD ANDA: ");
+    fgets(password, sizeof(password), stdin);
+
+    FILE *fptr = NULL;
+    fptr = fopen(Path, "r");
+    if(fptr == NULL){
+        system("cls");
+    }else{
+        while(fscanf(fptr,"%s", email) == 0){
+            if(strcmp(email, "1") == 0){
+                fscanf(fptr,"%s", password);
+                if(strcmp(password, "1") == 0){
+                    system("cls");
+                    printf("Sign In Anda Berhasil \n");
+                    exit(0);
+                }
+            }
+        }
+        system("cls");
+        printf("EMAIL DAN PASSWORD ANDA SALAH. COBA SEKALI LAGI ATAU PERGI KE MENU Sign Up\n");
+    }
 }
