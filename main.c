e2fe2435d63aab1538b5e2002071ecb4e1870939
Dyooooou <juliandiokhusussekolah@gmail.com>
2024-11-25 10:59:07 +0700

    pake switch

--- main.c
+++ main.c
@@ -1,58 +1,30 @@
-#include <stdio.h>
+#include<stdio.h>
+#include "film.h"
 
-void MenampilkanFilm();
-int PilihFilm();
-void MenampilkanLokasi();
-int PilihLokasi();
-void MenampilkanJadwal();
-int PilihJadwal();
-void konfirmasiPilihan(int PilihanFilm, int PilihanLokasi, int PilihanJadwal);
-
-int main (){
-//   printf("Hellooooo TUGAS");
-// return 0;
-// julian bukan jawa
-    int PilihanFilm, PilihanLokasi, PilihanJadwal;
+int main(){
+    int PilihanFilm;
     MenampilkanFilm();
-    PilihanFilm = PilihFilm();
-    PilihanLokasi = PilihLokasi();
-    PilihanJadwal = PilihJadwal():
-    konfirmasiPilihan(int PilihanFilm, int PilihanLokasi, int PilihanJadwal);
-}
+    printf("Film apa yang ingin anda tonton (1/2): ");
+    scanf("%d", &PilihanFilm);
+    switch (PilihanFilm){
+        case 1:
+            FilmPertama();
+            MenampilkanLokasi();
+            PilihLokasi();
+            MenampilkanJadwal(1);
+            PilihJadwal(1);
+            break;
+        case 2:
+            FilmKedua();
+            MenampilkanLokasi();
+            PilihLokasi();
+            MenampilkanJadwal(2);
+            PilihJadwal(2);
+            break;
+        default:
+            printf("Pilihan tidak valid. Silahkan coba lagi");
+            break;
+        }
 
-void MenampilkanFilm();
-
-void MenampilkanFilm(){
-    printf("Daftar Film yang dapat ditonton /n");
-    printf("1. Oshi No Ko");
-    printf("2. The Last of Us");
-    printf("3. DAYZ GONE");
-}
-
-int PilihFilm(){
-    int Pilihan;
-    printf("pilih no film yang dinginkan: ");
-    scanf("%d", &Pilihan);
-    return Pilihan;
-}
-
-void MenampilkanLokasi(){
-  printf("Film ingin di tonton dimana /n");
-  printf("1. CGV");
-  printf("2. XXI");
-}
-
-int PilihLokasi(){
-  int Pilihan;
-  printf("Pilih tempat untuk menonton: ");
-  scanf("%d", &Pilihan);
-  return Pilihan;
-}
-void MenampilkanJadwal(){
-  printf("pilih jadwal yang anda inginkan: ");
-  printf("1. 9.30");
-  printf("2. 12.00");
-  printf("3. 15.00");
-  printf("4. 18.30");
-  printf("5. 22.00");
+    return 0;
 }
