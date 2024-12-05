18536750d232909212d2ce11bd5525c936b0796e
Kura <akinariie@gmail.com>
2024-12-05 13:32:47 +0700

    Perkursian.c

--- Perkursian.c
+++ Perkursian.c
@@ -1,22 +1,13 @@
-/* main program belum diganti jadi modul
-yg harus ada di main program:
-printf ("Mau memesan atas nama siapa?\n");
-scan f("%s, nama"); 
-printf("\Masukkan nomor kursi yang kamu mau.\n");
-scanf("%d", &nomor_kursi);
-*/
-
 #include <stdio.h>
-
-typedef struct {
-    char nama[20];
-    int nomor_kursi;
-} Reservasi;
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "Perkursian.h"
 
 Reservasi reservasi[100];
 int num_reservasi = 0;
 
-int main () {
+void tampilkan_kursi () {
     int brs, klm;
     printf("Pilih kursi yang Anda mau\nX = Kursi sudah terisi\n\n");
     for (brs = 0; brs < 6; brs++) {
@@ -42,11 +33,11 @@ int main () {
     printf("\n");
 }
 
-void reservasi_kursi(char name[], int nomor_kursi) {
+void reservasi_kursi(char nama[], int nomor_kursi) {
 	if (num_reservasi >= 100) {
 		printf("Maaf, kursi sudah penuh.\n");
 	}
-	for (int i=0, i<num_reservasi, i++) {
+	for (int i = 0; i < num_reservasi; i++) {
 		if (reservasi[i].nomor_kursi == nomor_kursi) {
 			printf("Maaf, kursi ini sudah direservasi.\n");
 		}
@@ -56,5 +47,23 @@ void reservasi_kursi(char name[], int nomor_kursi) {
 	num_reservasi++;
 	
 	printf("Kursi %d sudah dipesan untuk %s. \n", nomor_kursi, nama);
-}
+} 
 
+void perkursian () {
+	char nama[20];
+	int nomor_kursi;
+	
+	system("cls");
+	tampilkan_kursi();
+	printf("\n\nMau memesan atas nama siapa?\n");
+	scanf("%s", &nama);
+	printf("\nMasukkan nomor kursi yang diinginkan\n");
+	scanf("%d", &nomor_kursi);
+	reservasi_kursi(nama, nomor_kursi);
+	sleep(2);
+	printf("\nSebentar lagi kembali ke halaman pemesanan kursi,\nSilahkan mengecek kursi anda\n");
+	sleep(4);
+	system("cls");
+	tampilkan_kursi();
+	sleep(5);
+} 
